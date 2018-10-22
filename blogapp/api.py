940cd1cc656b1b17894e4ab5dd5db56369cf69e3
Pan <dengpan.yuan.cool@gmail.com>
2018-10-21 22:07:33 -0400

    push 2.0

--- blogapp/api.py
+++ blogapp/api.py
@@ -1,17 +1,18 @@
 from blogapp.models import Article
 from rest_framework import serializers
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from rest_framework.decorators import api_view
 
 
-class ArticleSerializer(serializers.ModelDurationField):
+class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'  # {'title', 'content'} yuanzu
 
 
 @api_view(['GET'])
-def article():
+def article(request):
     article_list = Article.objects.all()
-    serializers = ArticleSerializer(article_list, many=True)
-    return JsonResponse(serializers.data)
+    serializer = ArticleSerializer(article_list, many=True)
+    # return JsonResponse(serializer.data)
+    return JsonResponse(serializer.data, safe=False)
